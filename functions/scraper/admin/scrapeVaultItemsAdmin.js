79588a900c775889f2b738d40bd9414134a2ad7d
Will Tesler <willtesler@gmail.com>
2022-04-17 18:27:12 -0400

    Remove scraper

--- functions/scraper/admin/scrapeVaultItemsAdmin.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-  const scrapeVaultItems = require("../scrapeVaultItems");
-  try {
-    const items = await scrapeVaultItems();
-    console.log(items);
-  } catch (e) {
-    console.error(e);
-  }
-})();
