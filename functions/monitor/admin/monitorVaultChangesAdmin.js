97e4a0243956930d3b40af67cb9674f262cfc714
Will Tesler <willtesler@gmail.com>
2022-04-17 18:28:32 -0400

    remove vault monitor

--- functions/monitor/admin/monitorVaultChangesAdmin.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-  const monitorVaultChanges = require("../monitorVaultChanges");
-  try {
-    await monitorVaultChanges();
-    console.log('Success');
-  } catch (e) {
-    console.error(e);
-  }
-})();
--- functions/vault/read/admin/readStoredVaultItemsAdmin.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-  const readStoredVaultItems = require("../readStoredVaultItems");
-  try {
-    const vaultItems = await readStoredVaultItems();
-    console.log(vaultItems);
-  } catch (e) {
-    console.error(e);
-  }
-})();
