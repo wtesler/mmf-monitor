3a3c6a9a8585348bf5508ffaf4fffa15c2a91f3d
Will Tesler <willtesler@gmail.com>
2022-05-04 18:19:17 -0400

    very accurate simple swap.

--- functions/web3/swap/liquidity/admin/swapBasicAdmin.js
+++ functions/web3/swap/liquidity/admin/swapBasicAdmin.js
@@ -1,17 +1,27 @@
 (async () => {
   const swapBasic = require("../swapBasic");
   const prepareWallet = require("../../../wallet/prepareWallet");
+  const readTokenBalance = require("../../../token/readTokenBalance");
   const TokenNames = require("../../../../constants/TokenNames");
+  const TokenDecimals = require("../../../../constants/TokenDecimals");
   const readDefiMnemonic = require('../../../../secrets/specific/readDefiMnemonic');
+  const {ethers} = require("ethers");
 
   const mnemonic = await readDefiMnemonic();
 
   const wallet = await prepareWallet(mnemonic);
 
+  const SRC_TOKEN = TokenNames.MUSD;
+  const DST_TOKEN = TokenNames.USDC;
+  // const AMOUNT_NUM = 5;
+  // const balanceBigNumber = ethers.utils.parseUnits(AMOUNT_NUM.toString(), TokenDecimals[SRC_TOKEN]);
+
+  const balanceBigNumber = await readTokenBalance(SRC_TOKEN, wallet);
+
   await swapBasic(
-    TokenNames.SVN,
-    TokenNames.MMF,
-    '0x017d62781d8fbf9a6abf',
+    SRC_TOKEN,
+    DST_TOKEN,
+    balanceBigNumber,
     wallet
   );
 })();
--- functions/web3/swap/liquidity/swapBasic.js
+++ functions/web3/swap/liquidity/swapBasic.js
@@ -1,19 +1,57 @@
 module.exports = async (srcToken, dstToken, srcInBigNumber, wallet) => {
+  const dexScreenerClient = require('../../../dexscreener/client/DexScreenerClient');
   const swapTokens = require('../swapTokens');
   const TokenAddresses = require("../../../constants/TokenAddresses");
+  const TokenDecimals = require("../../../constants/TokenDecimals");
+  const NetworkNames = require("../../../constants/NetworkNames");
 
-  const ACTION = `SWAP LIQUIDITY`;
+  const ACTION = `SWAP BASIC`;
 
   console.log(`${ACTION} | TOKENS: ${srcToken} / ${dstToken}`);
 
-  console.log(`${ACTION} | SWAPPING ${srcInBigNumber.toString()} ${srcToken} for atleast 0 ${dstToken}.`);
-
   const parameterFunction = async() => {
+    let address = TokenAddresses[`${srcToken}_${dstToken}`];
+    if (!address) {
+      address = TokenAddresses[`${dstToken}_${srcToken}`];
+    }
+    const pairInfo = await dexScreenerClient.readPairInfo(NetworkNames.CRONOS, address);
+    const pair = pairInfo.pair;
+    const quoteToken = pair.quoteToken.symbol;
+    const baseToken = pair.baseToken.symbol;
+    const liquidity = pair.liquidity;
+    const lQuote = liquidity.quote;
+    const lBase = liquidity.base;
+    // console.log(`${quoteToken}, ${baseToken}, ${lQuote}, ${lBase}`)
+    const isQuoteSrc = quoteToken === srcToken;
+    const priceNative = (isQuoteSrc ? lBase : lQuote) / (isQuoteSrc ? lQuote : lBase);
+
+    const slippage = .998;
+    let roundingNum;
+    let decimalAdjustment = Math.pow(10, TokenDecimals[dstToken]) / Math.pow(10, TokenDecimals[srcToken]);
+    if (decimalAdjustment < 1) {
+      roundingNum = Math.floor(1 / decimalAdjustment);
+      decimalAdjustment = 1;
+      console.log(roundingNum);
+    } else {
+      roundingNum = 1;
+    }
+
+    console.log(decimalAdjustment);
+
+   //  const dstOutMinBigNumber = '0x0';
+    const dstOutMinBigNumber = srcInBigNumber
+      .mul(decimalAdjustment)
+      .div(roundingNum)
+      .mul(Math.floor(slippage * priceNative * 10000))
+      .div(10000)
+
+    console.log(`${ACTION} | Swapping ${srcInBigNumber.toString()} ${srcToken} for atleast ${dstOutMinBigNumber.toString()} ${dstToken}.`);
+
     const internalTransactions = [
       TokenAddresses[srcToken], TokenAddresses[dstToken]
     ];
 
-    return [srcInBigNumber, '0x0', internalTransactions];
+    return [srcInBigNumber, dstOutMinBigNumber, internalTransactions];
   };
 
   await swapTokens(parameterFunction, wallet);
